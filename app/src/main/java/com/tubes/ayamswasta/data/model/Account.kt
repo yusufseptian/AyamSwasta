848aa8bde1424111bc77061251aeeafd35c80269
Wahyudistira <wahyudistiragunners@gmail.com>
2022-05-22 05:13:04 +0700

    retrofit

---
+++ app/src/main/java/com/tubes/ayamswasta/data/model/Account.kt
@@ -0,0 +1,9 @@
+package com.tubes.ayamswasta.data.model
+
+data class Account(
+    val us_username : String,
+    val us_fullname : String,
+    val us_alamat : String,
+    val us_telepon : String,
+    val us_role_id : String
+)
