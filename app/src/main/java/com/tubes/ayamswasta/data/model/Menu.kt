a05d823c4cc3342f4878781ba60edbd5871c29f9
mndmj <nasrullah.03.arul@gmail.com>
2022-05-29 03:20:01 +0700

    screen login

--- app/src/main/java/com/tubes/ayamswasta/data/model/Menu.kt
+++
@@ -1,9 +0,0 @@
-package com.tubes.ayamswasta.data.model
-
-data class Menu(
-    val menu_id : String,
-    val menu_name : String,
-    val menu_harga : Long,
-    val menu_disc : Int,
-    val menu_status : String
-)
