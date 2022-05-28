3d3c1410fe57b7488c74a5b7b6e1875c495a1123
NaufalPujiMahdy <naufalpm230800@gmail.com>
2022-05-29 04:36:38 +0700

    test

--- app/src/main/java/com/tubes/ayamswasta/MainActivity.kt
+++ app/src/main/java/com/tubes/ayamswasta/MainActivity.kt
@@ -19,25 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-        val loginTest = Repository.login("yudi","yudist").enqueue(object : Callback<List<Login>>{
-            override fun onResponse(call: Call<List<Login>>, response: Response<List<Login>>) {
-                if(response.isSuccessful){
-                    response.body()?.forEach {
-                        findViewById<TextView>(R.id.lblTest).setText(it.us_role_id.toString())
-                    }
-                } else {
-                    findViewById<TextView>(R.id.lblTest).setText("Error : " + response.code())
-                }
-            }
-
-            override fun onFailure(call: Call<List<Login>>, t: Throwable) {
-                findViewById<TextView>(R.id.lblTest).setText("s_Error : " + t.message)
-            }
-
-        })
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
         navController = navHostFragment.navController
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
