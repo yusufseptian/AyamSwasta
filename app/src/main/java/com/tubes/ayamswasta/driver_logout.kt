e66427ef8bc1f3e91750be406e4eabcf00f5be79
mndmj <nasrullah.03.arul@gmail.com>
2022-05-29 22:52:52 +0700

    hoo

---
+++ app/src/main/java/com/tubes/ayamswasta/driver_logout.kt
@@ -0,0 +1,11 @@
+package com.tubes.ayamswasta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class driver_logout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_driver_logout)
+    }
+}
