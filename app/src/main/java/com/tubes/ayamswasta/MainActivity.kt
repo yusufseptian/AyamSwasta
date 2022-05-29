4209fc603c588c4a30f4ffeaacf4efbde34a4828
NaufalPujiMahdy <naufalpm230800@gmail.com>
2022-05-29 16:15:25 +0700

    mainactivity

--- app/src/main/java/com/tubes/ayamswasta/MainActivity.kt
+++ app/src/main/java/com/tubes/ayamswasta/MainActivity.kt
@@ -20,34 +20,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val action = MenuMakananRepository.insertMenu()
-        action.insertMenu(
-            "Bakpia","45000","0","Ada"
-        ).enqueue(object:Callback<ResponseAction>{
-            override fun onResponse(
-                call: Call<ResponseAction>,
-                response: Response<ResponseAction>
-            ) {
-                if(response.isSuccessful){
-//                    response.body()?.forEach {
-//                        findViewById<TextView>(R.id.lblTest).text = it.messages
-//                    }\
-                    findViewById<TextView>(R.id.lblTest).text = response.body()!!.messages
-                } else {
-                    findViewById<TextView>(R.id.lblTest).text = "err code : "+response.code().toString()
-                }
-            }
-
-            override fun onFailure(call: Call<ResponseAction>, t: Throwable) {
-                findViewById<TextView>(R.id.lblTest).text = t.toString()
-            }
-
-        })
-
-//
-//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-//        navController = navHostFragment.navController
-//        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-//        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
